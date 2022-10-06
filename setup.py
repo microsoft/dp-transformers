7e5c0e489c0c4a3fb510a4491521297c6cde0a6c
Daniel <jonesdaniel@microsoft.com>
2022-10-06 08:20:06 +0000

    changing setup to depend on datasets >= 2.0.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     },
     install_requires=[
         "transformers>=4.20.1",
-        "datasets<2.0.0",
+        "datasets>=2.0.0",
         "opacus<1.0.0",
         "prv-accountant<0.2.0",
         "torch<1.10"
