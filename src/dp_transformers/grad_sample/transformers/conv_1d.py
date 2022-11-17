5efcd8b7d6d6ee34259907b6c4fd3c063a1e2528
Huseyin Atahan Inan <89604977+huseyinatahaninan@users.noreply.github.com>
2022-11-17 10:07:39 -0800

    use opt_einsum with the new version of Opacus (#20)
    Just as replaced in the new version of Opacus, here we switch to using einsum from https://optimized-einsum.readthedocs.io/en/stable/ that can significantly reduce the overall execution time of einsum-like expressions by optimizing the expression’s contraction order and dispatching many operations to canonical BLAS, cuBLAS, or other specialized routines (quoted from the website).

--- src/dp_transformers/grad_sample/transformers/conv_1d.py
+++ src/dp_transformers/grad_sample/transformers/conv_1d.py
@@ -5,6 +5,8 @@ from typing import Dict
 
 import torch
 import torch.nn as nn
+from opt_einsum import contract
+
 from opacus.grad_sample.utils import register_grad_sampler
 
 from transformers.modeling_utils import Conv1D
@@ -14,7 +16,7 @@ def compute_transformers_conv1d_grad_sample(
     layer: Conv1D, A: torch.Tensor, B: torch.Tensor
 ) -> Dict[nn.Parameter, torch.Tensor]:
     ret = {}
-    ret[layer.weight] = torch.einsum("n...i,n...j->nji", B, A).contiguous()
+    ret[layer.weight] = contract("n...i,n...j->nji", B, A).contiguous()
     if layer.bias is not None:
-        ret[layer.bias] = torch.einsum("n...k->nk", B)
+        ret[layer.bias] = contract("n...k->nk", B)
     return ret
