edf7a0f23adb990a7770ca63a67abcaddac30342
Huseyin Atahan Inan <Huseyin.Inan@microsoft.com>
2023-02-01 10:35:03 -0800

    moe without DP

---
+++ examples/nlg-reddit-moe/fine-tune-nodp.py
@@ -0,0 +1,99 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+'''Train MoE model series without DP'''
+
+import datasets
+import dp_transformers
+import transformers
+import sys
+import logging
+
+from dataclasses import dataclass, field
+from transformers import AutoTokenizer, AutoConfig, SwitchTransformersForConditionalGeneration
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    model_name: str = field(default="gpt2", metadata={
+        "help": "Model name in HuggingFace, e.g. 'gpt2'"
+    })
+
+    sequence_len: int = field(default=128, metadata={
+        "help": "Model sequence length"
+    })
+
+
+@dataclass
+class Arguments:
+    train: dp_transformers.TrainingArguments
+    model: ModelArguments
+
+
+def main(args: Arguments):
+    transformers.set_seed(args.train.seed)
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    log_level = train_args.get_process_log_level()
+    logger.setLevel(log_level)
+    datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {train_args.local_rank}, device: {train_args.device}, n_gpu: {train_args.n_gpu}, "
+        f"distributed training: {bool(train_args.local_rank != -1)}, 16-bits training: {train_args.fp16}"
+    )
+    logger.info(f"Training/evaluation parameters {train_args}")
+
+    # Load model
+    model = SwitchTransformersForConditionalGeneration.from_pretrained(args.model.model_name)
+    model = model.to(train_args.device)
+
+    # Load data
+    dataset = datasets.load_dataset('reddit', split="train[:500000]").train_test_split(0.02, seed=args.train.seed)
+
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model.model_name)
+    tokenizer.pad_token = -100 # Set a dummy pad token we don't use it anyway
+
+    # Tokenize data
+    with train_args.main_process_first(desc="tokenizing dataset"):
+        dataset = dataset.map(
+            lambda batch: tokenizer(batch['content'], padding="max_length", truncation=True, max_length=args.model.sequence_len),
+            batched=True, num_proc=8, desc="tokenizing dataset", remove_columns=dataset.column_names['train']
+        )
+
+    if train_args.local_rank == 0:
+        logger.info(f"Total number of parameters of the model: {model.num_parameters(only_trainable=False)}")
+        logger.info(f"Fine-tuned number of parameters of the model: {model.num_parameters(only_trainable=True)}")
+
+    model = model.cuda()
+    model.train()
+
+    data_collator = dp_transformers.DataCollatorForPrivateCausalLanguageModeling(tokenizer)
+
+    trainer = transformers.Trainer(
+        args=train_args,
+        model=model,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'],
+        data_collator=data_collator
+    )
+
+    trainer.train()
+
+if __name__ == "__main__":
+    arg_parser = transformers.HfArgumentParser((dp_transformers.TrainingArguments, ModelArguments))
+    train_args, model_args = arg_parser.parse_args_into_dataclasses()
+    main(Arguments(train=train_args, model=model_args))
