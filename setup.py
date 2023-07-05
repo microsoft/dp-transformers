36d32fc0b976dd4ea115f9b1a9fc0dc83209b2f8
Huseyin Atahan Inan <Huseyin.Inan@microsoft.com>
2023-07-05 08:31:51 -0700

    fix package versions

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
         ]
     },
     install_requires=[
-        "transformers>=4.20.1",
+        "transformers>=4.20.1,<=4.28.1",
         "datasets>=2.0.0,<=2.6.1",
-        "opacus>=1.2.0,<1.3.0",
+        "opacus>=1.1.3,<1.3.0",
         "prv-accountant<0.2.0",
         "torch>=1.9.1,<=1.12.1",
     ],
