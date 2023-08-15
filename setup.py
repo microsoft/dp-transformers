7b507af05257a192b48263518f72c722f3ded072
Lukas Wutschitz <luwutsch@microsoft.com>
2023-08-15 09:23:06 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=[
         "transformers>=4.20.1,<=4.28.1",
         "datasets>=2.0.0,<=2.6.1",
-        "opacus>=1.1.3,<1.3.0",
+        "opacus>=1.3.0",
         "prv-accountant<0.2.0",
         "torch>=1.9.1,<=1.13.1",
     ],
