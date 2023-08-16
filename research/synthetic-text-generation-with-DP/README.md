04c238c17a994f3caf99a5cebadee12c7430de68
Huseyin Atahan Inan <89604977+huseyinatahaninan@users.noreply.github.com>
2023-08-16 11:35:26 -0700

    Huinan/synthetic text generation with dp (#29)
    * initial commit
    
    * fixes
    
    * add a note about lora to the readme
    
    * fix package versions, add notice and update readme for 3rd party code
    
    ---------
    
    Co-authored-by: Huseyin Atahan Inan <huinan@microsoft.com>

---
+++ research/synthetic-text-generation-with-DP/README.md
@@ -0,0 +1,126 @@
+We present the code of our paper "Synthetic Text Generation with Differential Privacy: A Simple and Practical Recipe" at ACL 2023.
+
+## Fine-tuning with DP
+
+The following script assumes distributed training on 8 GPUs.
+
+```console
+python -m torch.distributed.run --nproc_per_node 8 fine-tune-dp.py \
+    --data_dir $DATA \
+    --output_dir $OUTPUT_DIR \
+    --model_name gpt2 \
+    --per_device_train_batch_size 32 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --log_level info \
+    --per_device_eval_batch_size 64 \
+    --eval_accumulation_steps 1 \
+    --seed 42 \
+    --target_epsilon 4.0 \
+    --per_sample_max_grad_norm 1.0 \
+    --weight_decay 0.01 \
+    --remove_unused_columns False \
+    --num_train_epochs 50 \
+    --logging_steps 10 \
+    --max_grad_norm 0 \
+    --sequence_len 128 \
+    --learning_rate 0.0001 \
+    --lr_scheduler_type constant \
+    --dataloader_num_workers 2 \
+    --disable_tqdm True \
+    --load_best_model_at_end True \
+```
+
+## Fine-tuning without DP
+
+The following script assumes distributed training on 8 GPUs.
+
+```console
+python -m torch.distributed.run --nproc_per_node 8 fine-tune-nodp.py \
+    --data_dir $DATA \
+    --output_dir $OUTPUT_DIR \
+    --model_name gpt2 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --log_level info \
+    --per_device_eval_batch_size 64 \
+    --eval_accumulation_steps 1 \
+    --seed 42 \
+    --weight_decay 0.01 \
+    --remove_unused_columns False \
+    --num_train_epochs 5 \
+    --logging_steps 2400 \
+    --max_grad_norm 0 \
+    --sequence_len 128 \
+    --learning_rate 0.00005 \
+    --lr_scheduler_type constant \
+    --dataloader_num_workers 2 \
+    --disable_tqdm True \
+    --load_best_model_at_end True \
+```
+
+## Synthetic Text Generation
+
+The following script generates synthetic data from a fine-tuned model on a single GPU.
+
+```console
+python generate-text.py \
+    --model_type gpt2 \
+    --model_name_or_path $CHECKPOINT_FOLDER \
+    --input_training_file $TRAINING_DATA_FILE \
+    --output_dir $OUTPUT_DIR \
+    --length 128 \
+    --total_sequences 100000 \
+    --do_sample \
+    --batch_size 8 \
+```
+
+## Classification model
+
+The following script assumes distributed training on 8 GPUs. 
+Set --sample_dataset True to train the classifier on the original data to sample 100000 data points.
+
+```console
+python -m torch.distributed.run --nproc_per_node 8 run-classification.py \
+    --model_name_or_path roberta-base \
+    --output_dir $OUTPUT_DIR \
+    --train_file $TRAINING_DATA_FILE \
+    --validation_file $VAL_DATA_FILE \
+    --test_file $TEST_DATA_FILE \
+    --do_train \
+    --do_eval \
+    --do_predict \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 64 \
+    --learning_rate 3e-5 \
+    --num_train_epochs 3 \
+    --logging_steps 100 \
+    --overwrite_output_dir \
+    --overwrite_cache True \
+    --evaluation_strategy steps \
+    --eval_steps 31 \
+    --save_steps 31 \
+    --load_best_model_at_end True \
+    --label_column_name "label1" \
+    --sample_dataset False \
+    --disable_tqdm True
+```
+
+## Using LoRA during fine-tuning
+
+Although not used in the paper, LoRA fine-tuning significantly improves the runtime by allowing much larger
+batch sizes to fit in each GPU. A starting point could be to add `--lora_dim 4 --lora_alpha 32 --lora_dropout 0.0`
+and use larger learning rates such as `--learning_rate 3e-4` or `4e-4`.
+
+## Third Party Notice
+
+This software includes the files listed below from the Huggingface/Transformers Library (https://github.com/huggingface/transformers) 
+as part of text generation and task performance.
+
+    research/synthetic-text-generation-with-DP
+    ├── generate-text.py
+    └── run-classification.py
---
+++ research/synthetic-text-generation-with-DP/fine-tune-dp.py
@@ -0,0 +1,165 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+'''Train GPT2 model series with DP (w/ parameter-efficient approach LoRA when lora_dim > 0)'''
+
+import os
+import datasets
+import dp_transformers
+import transformers
+import sys
+import logging
+
+from dataclasses import dataclass, field
+from dp_transformers.layers.dp_merged_linear import mark_only_lora_as_trainable
+from dp_transformers.module_modification import convert_gpt2_attention_to_lora
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    data_dir: str = field(default="./", metadata={
+        "help": "Path to training data"
+    })
+
+    model_name: str = field(default="gpt2", metadata={
+        "help": "Model name in HuggingFace, e.g. 'gpt2'"
+    })
+
+    lora_dim: int = field(default=0, metadata={
+        "help": "LoRA dimension; 0 means LoRA is disabled"
+    })
+
+    sequence_len: int = field(default=128, metadata={
+        "help": "Model sequence length"
+    })
+
+    lora_dropout: float = field(default=0.0, metadata={
+        "help": "Dropout probability for LoRA layers"
+    })
+
+    lora_alpha: int = field(default=32, metadata={
+        "help": "LoRA attention alpha"
+    })
+
+@dataclass
+class Arguments:
+    train: dp_transformers.TrainingArguments
+    privacy: dp_transformers.PrivacyArguments
+    model: ModelArguments
+
+
+def main(args: Arguments):
+
+    transformers.set_seed(args.train.seed)
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    log_level = train_args.get_process_log_level()
+    logger.setLevel(log_level)
+    datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {train_args.local_rank}, device: {train_args.device}, n_gpu: {train_args.n_gpu}, "
+        f"distributed training: {bool(train_args.local_rank != -1)}, 16-bits training: {train_args.fp16}"
+    )
+    logger.info(f"Training/evaluation parameters {train_args}")
+    logger.info(f"Privacy parameters {privacy_args}")
+
+    # Load model
+    model = transformers.AutoModelForCausalLM.from_pretrained(args.model.model_name)
+    model = model.to(train_args.device)
+
+    # Load data
+    data_path_train = os.path.join(args.model.data_dir, "train.csv")
+    data_path_val = os.path.join(args.model.data_dir, "val.csv")
+    dataset = datasets.load_dataset('csv', data_files={'train': data_path_train, 'validation': data_path_val})
+
+    # Load tokenizer
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model.model_name)
+    num_added_toks = tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    mean_tok_emb = model.transformer.wte.weight.data.mean(dim=0)
+    model.resize_token_embeddings(len(tokenizer))
+
+    # Initialize the newly-added token embedding to the mean of all token embeddings
+    for i in range(num_added_toks):
+        model.transformer.wte.weight.data[-(i + 1), :] = mean_tok_emb
+
+    label_column_names = [name for name in dataset["train"].column_names if "label" in name]
+
+    # Tokenize data
+    def preprocess_function(examples):
+        batch = []
+        for t in range(len(examples['text'])):
+            text = "\t".join([examples[name][t] for name in label_column_names]) + "\n\n" + examples['text'][t] + tokenizer.eos_token
+            batch.append(text)
+
+        result = tokenizer(batch, padding="max_length", truncation=True,
+                           max_length=args.model.sequence_len)
+
+        return result
+
+    # Tokenize data
+    with train_args.main_process_first(desc="tokenizing dataset"):
+        dataset = dataset.map(
+            preprocess_function, batched=True, desc="tokenizing dataset", remove_columns=dataset.column_names['train']
+        )
+
+    if args.model.lora_dim > 0:
+        model = convert_gpt2_attention_to_lora(
+            model, r=args.model.lora_dim, lora_alpha=args.model.lora_alpha, lora_dropout=args.model.lora_dropout,
+            enable_lora=[True, False, True], merge_weights=False
+        )
+        mark_only_lora_as_trainable(model)
+
+    if train_args.local_rank == 0:
+        logger.info(f"Total number of parameters of the model: {model.num_parameters(only_trainable=False)}")
+        logger.info(f"Fine-tuned number of parameters of the model: {model.num_parameters(only_trainable=True)}")
+
+    model = model.cuda()
+    model.train()
+
+    data_collator = dp_transformers.DataCollatorForPrivateCausalLanguageModeling(tokenizer)
+
+    trainer = dp_transformers.dp_utils.OpacusDPTrainer(
+        args=train_args,
+        model=model,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['validation'],
+        data_collator=data_collator,
+        privacy_args=privacy_args,
+        tokenizer=tokenizer
+    )
+
+    try:
+        train_result = trainer.train()
+    finally:
+        eps_prv = trainer.get_prv_epsilon()
+        eps_rdp = trainer.get_rdp_epsilon()
+        trainer.log({
+            "final_epsilon_prv": eps_prv,
+            "final_epsilon_rdp": eps_rdp
+        })
+
+    if train_args.local_rank == 0 or train_args.local_rank == -1:
+        metrics = train_result.metrics
+        trainer.save_model()
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+
+
+if __name__ == "__main__":
+    arg_parser = transformers.HfArgumentParser((dp_transformers.TrainingArguments, dp_transformers.PrivacyArguments, ModelArguments))
+    train_args, privacy_args, model_args = arg_parser.parse_args_into_dataclasses()
+    main(Arguments(train=train_args, privacy=privacy_args, model=model_args))
