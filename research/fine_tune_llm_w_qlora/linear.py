52f3fcaff3b54f925bb79ead16f9a39ca08a8d4e
huseyinatahaninan <Huseyin.Inan@microsoft.com>
2023-12-18 18:12:18 -0800

    Fine-tune LLMs using QLoRA with and without DP.

---
+++ research/fine_tune_llm_w_qlora/linear.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, List
+
+import torch
+import torch.nn as nn
+from opt_einsum import contract
+
+from opacus.grad_sample.utils import register_grad_sampler
+
+
+@register_grad_sampler(nn.Linear)
+def compute_linear_grad_sample(
+    layer: nn.Linear, activations: List[torch.Tensor], backprops: torch.Tensor
+) -> Dict[nn.Parameter, torch.Tensor]:
+    """
+    Computes per sample gradients for ``nn.Linear`` layer
+
+    Args:
+        layer: Layer
+        activations: Activations
+        backprops: Backpropagations
+    """
+    activations = activations[0]
+    ret = {}
+    if layer.weight.requires_grad:
+        gs = contract("n...i,n...j->nij", backprops.float(), activations.float())
+        ret[layer.weight] = gs
+    if layer.bias is not None and layer.bias.requires_grad:
+        ret[layer.bias] = contract("n...k->nk", backprops.float())
+    return ret
